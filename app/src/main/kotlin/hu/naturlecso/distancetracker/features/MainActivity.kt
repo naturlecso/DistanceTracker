980fbf7d7014a49edc53b7e1e1287f726bc0db00
Jozsef Szilvasi <jozsef.szilvasi@gmail.com>
2019-10-05 18:48:53 +0200

    Add permission check

--- app/src/main/kotlin/hu/naturlecso/distancetracker/features/MainActivity.kt
+++ app/src/main/kotlin/hu/naturlecso/distancetracker/features/MainActivity.kt
@@ -13,9 +13,17 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
+import android.content.pm.PackageManager
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import androidx.core.app.ActivityCompat
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import com.google.android.material.snackbar.Snackbar
+import hu.naturlecso.distancetracker.BuildConfig
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-
     private val navigator: Navigator by inject()
 
     private lateinit var disposable: Disposable
@@ -36,11 +44,77 @@ class MainActivity : AppCompatActivity() {
                     is NavigationCommand.Back -> navController.navigateUp()
                 }
             }
+
+        if (!checkPermissions()) {
+            requestPermissions()
+        }
     }
 
     override fun onDestroy() {
         disposable.disposeIfNeeded()
 
         super.onDestroy()
+    }
+
+    private fun checkPermissions(): Boolean {
+        return REQUIRED_PERMISSIONS
+            .map { ActivityCompat.checkSelfPermission(this, it) }
+            .map { it == PackageManager.PERMISSION_GRANTED }
+            .all { it }
+    }
+
+    private fun requestPermissions() {
+        val allPermissionsApproved = checkPermissions()
+
+        if (allPermissionsApproved) {
+            Snackbar.make(container, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                .setAction(android.R.string.ok) { triggerPermissionRequest() }
+                .show()
+        } else {
+            triggerPermissionRequest()
+        }
+    }
+
+    private fun triggerPermissionRequest() {
+        ActivityCompat.requestPermissions(
+            this,
+            REQUIRED_PERMISSIONS,
+            REQUEST_PERMISSIONS_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            when {
+                grantResults.isEmpty() -> Timber.i("User interaction was cancelled.")
+                grantResults.map { it == PackageManager.PERMISSION_GRANTED }.all { it } -> {
+                    // TODO start action
+                }
+                else -> Snackbar.make(container, R.string.permission_denied_explanation, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.settings) {
+                        // Build intent that displays the App settings screen.
+                        val intent = Intent().apply {
+                            action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            data = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
+                        startActivity(intent)
+                    }
+                    .show()
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+
+        // TODO maybe ACCESS_BACKGROUND_LOCATION is needed too
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            ACCESS_FINE_LOCATION
+        )
     }
 }
